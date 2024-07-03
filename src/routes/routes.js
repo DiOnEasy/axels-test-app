6b413be1f151c404a3c7ab282a4deb05f0bee317
DiOnEasy <d11062002d@gmail.com>
2024-07-03 08:51:34 +0300

    configured snapshot saving in one folder

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
-import React, { Navigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 import { GalleryPage } from 'pages/GalleryPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
